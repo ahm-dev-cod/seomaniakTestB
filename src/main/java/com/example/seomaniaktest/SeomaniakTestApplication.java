50caec36c3002a1e5d6f0920084b814fef8724b4
Ahmed Debbab <ahmed.debbab.dev@gmail.com>
2025-03-07 10:48:37 +0000

    Initial commit

---
+++ src/main/java/com/example/seomaniaktest/SeomaniakTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.seomaniaktest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeomaniakTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeomaniakTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/seomaniaktest/SeomaniakTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seomaniaktest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeomaniakTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
