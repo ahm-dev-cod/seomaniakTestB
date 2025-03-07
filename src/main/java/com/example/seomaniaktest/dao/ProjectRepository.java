1fda12b3054998cc1669895827ecdd0850d0b865
Ahmed Debbab <ahmed.debbab.dev@gmail.com>
2025-03-07 14:14:25 +0000

    l ajout des deux methodes findWithTaskById dans PR et findByProject_Id dans TR

--- src/main/java/com/example/seomaniaktest/dao/ProjectRepository.java
+++ src/main/java/com/example/seomaniaktest/dao/ProjectRepository.java
@@ -1,7 +1,13 @@
 package com.example.seomaniaktest.dao;
 
 import com.example.seomaniaktest.entities.Project;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProjectRepository extends JpaRepository<Project, Long> {
+    @EntityGraph(attributePaths = {"tasks"})
+    Optional<Project> findWithTasksById(Long id);
+
 }
--- src/main/java/com/example/seomaniaktest/dao/TaskRepository.java
+++ src/main/java/com/example/seomaniaktest/dao/TaskRepository.java
@@ -3,5 +3,8 @@ package com.example.seomaniaktest.dao;
 import com.example.seomaniaktest.entities.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findByProject_Id(Long projectId);
 }
